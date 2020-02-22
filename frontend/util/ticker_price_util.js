80ed13fe7df3770c1e468c28e4b0e0e90d49b45d
chels <chemccann@hotmail.com>
2020-02-21 22:09:47 -0500

    API Swithc

--- frontend/util/ticker_price_util.js
+++ frontend/util/ticker_price_util.js
@@ -2,8 +2,8 @@
 export const fetchPrice = (ticker) => {
     return $.ajax({
         method: "GET", 
-        url: `https://cloud.iexapis.com/stable/stock/${ticker}/price?token=${ENV["MY_API_KEY"]}`,
-        // url: `https://sandbox.iexapis.com/stable/stock/${ticker}/price?token=Tpk_4ca09027bbda4ce1a28d8e1702fafdaa`,
+        // url: `https://cloud.iexapis.com/stable/stock/${ticker}/price?token=${ENV["MY_API_KEY"]}`,
+        url: `https://sandbox.iexapis.com/stable/stock/${ticker}/price?token=${ENV["MY_API_KEY"]}`,
     })
 }
 
@@ -11,15 +11,15 @@ export const fetchOpenPrice = (ticker) => {
     return $.ajax({
         method: "GET", 
         // url: `https://cloud.iexapis.com/stable/stock/${ticker}/ohlc?token=pk_b6f890a95fb24dbfb1a85f362fe5687f`,
-        url: `https://sandbox.iexapis.com/stable/stock/${ticker}/ohlc?token=Tpk_4ca09027bbda4ce1a28d8e1702fafdaa`,
+        url: `https://sandbox.iexapis.com/stable/stock/${ticker}/ohlc?token=${ENV["MY_API_KEY"]}`,
     })
 }
 
 export const fetchPrices = ticker => {
     return $.ajax({
         method: "GET",
-        url: `https://cloud.iexapis.com/stable/stock/${ticker}/intraday-prices?token=${ENV["MY_API_KEY"]}`
-        // url: `https://sandbox.iexapis.com/stable/stock/${ticker}/intraday-prices?token=Tpk_4ca09027bbda4ce1a28d8e1702fafdaa`
+        // url: `https://cloud.iexapis.com/stable/stock/${ticker}/intraday-prices?token=${ENV["MY_API_KEY"]}`
+        url: `https://sandbox.iexapis.com/stable/stock/${ticker}/intraday-prices?token=${ENV["MY_API_KEY"]}`
         
     })
 }
